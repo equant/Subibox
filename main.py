59ca63c50492ebe6d6e597d814158f9c3a87353e
Nathanial Hendler <nathanhendler@gmail.com>
2017-06-07 10:17:13 -0700

    tau

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+#from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 
@@ -20,31 +23,64 @@ analyzer    = MusicSearch.StringAnalyzer()
 # you can control the ScreenManager from kv. Each screen has by default a
 # property manager that gives you the instance of the ScreenManager used.
 Builder.load_string("""
+<FancyLabel>:
+    pos_hint: {'x': 0.1, 'y': 0.5}
+    size_hint_y: 0.15
+    size_hint_x: 0.15
+    canvas:
+        Color:
+            rgba: self.background_color
+        Rectangle:
+            pos: self.pos  # incorrect
+            size: self.size
+    Label:
+        text: self.text
+        color: [0,0,0,1]
+        font_size: 44
+
+<AlbumArt>:
+    orientation: "vertical"
+    Image:
+        source: 'cover.jpg'
+        #y: self.parent.y + self.parent.height - 200
+        #x: self.parent.x
+    FancyLabel:
+        
 <AlbumScreen>:
     id: album_screen
     GridLayout:
-        id: button_layout
+        id: album_layout
         rows: 3
-        Button:
-            text: '$'
-        Button:
-            text: '%'
-        Button:
-            text: 'C'
-        Button:
-            text: '!'
-        Button:
-            text: '@'
-        Button:
-            text: 'F'
-        Button:
-            text: '#'
-        Button:
-            text: '*'
-        Button:
-            id: next_button
-            text: 'Back To Main Menu'
-            on_release: root.manager.current = 'menu'
+
+        AlbumArt:
+            id: 1
+            text: "1"
+        AlbumArt:
+            id: 2
+            text: "2"
+        AlbumArt:
+            id: 3
+            text: "3"
+
+        AlbumArt:
+            id: 4
+            text: "4"
+        AlbumArt:
+            id: 5
+            text: "5"
+        AlbumArt:
+            id: 6
+            text: "6"
+
+        AlbumArt:
+            id: 7
+            text: "7"
+        AlbumArt:
+            id: 8
+            text: "8"
+        AlbumArt:
+            id: 9
+            text: "9"
 
 <LibraryScreen>:
     id: library_screen
@@ -74,6 +110,17 @@ Builder.load_string("""
             on_release: root.manager.current = 'menu'
 """)
 
+class AlbumArt(RelativeLayout):
+    def build(self):
+        return self
+
+
+class FancyLabel(Label):
+    background_color = ListProperty([1,0,0,0.8])
+    text = StringProperty("A")
+    def build(self):
+        return self
+
 
 class ProtoScreen(Screen):
 
@@ -98,8 +145,9 @@ class LibraryScreen(ProtoScreen):
 
         if input_string is None:
             self.switch_to_album_screen()
-        else:
-            self.search_string += input_string
+            return
+
+        self.search_string += input_string
         result = musicSearch.artist_search(self.search_string)
         if result is None:
             self.search_string = ""
@@ -122,23 +170,26 @@ class SettingsScreen(ProtoScreen):
 
 
 class AlbumScreen(ProtoScreen):
-    albums = ListProperty()
+    """
+    Album Dataframe: id, full_album_name, album_year, album_path, album_art
+    """
+
+    albums      = None
     album_pages = None
+    page = 1
 
     def __init__(self, *args, **kwargs):
         super(AlbumScreen, self).__init__(*args, **kwargs)
-        self.bind(albums=self.new_albums)
+        #self.bind(albums=self.new_albums)
 
-    def on_enter(self):
-        grid_layout_widget = self.ids.button_layout
-        print("Rows: {}".format(grid_layout_widget.rows))
-        for button in self.walk():
-            print("{} -> {}".format(button, button.id))
-            button.color = [random.random() for _ in range(3)] + [1]
+    def on_pre_enter(self):
+        if self.album_pages is None:
+            self.make_album_pages(self.albums)
+        grid_layout_widget = self.ids.album_layout
         #for button, string in zip(reversed(self.ids.button_layout.children), self.albums):
-        for button, i in zip(self.ids.button_layout.children, range(len(self.ids.button_layout.children))):
+        for album, i in zip(grid_layout_widget.children, range(len(grid_layout_widget.children))):
             if self.album_pages.iloc[i] is not None:
-                button.text = self.album_pages.iloc[i][1]
+                album.source = self.album_pages.iloc[i][4]
 
     def new_albums(self, a, b):
         print("DEBUG: Running bind method for AlbumScreen.albums")
@@ -154,9 +205,17 @@ class AlbumScreen(ProtoScreen):
          [10, 11, 12, 13, 14, 15, 16, 17, 18],
          [19, 20]]
         """
-        n = 9   # Number of albums per page.
-        pages = [a[x:x+n] for x in range(0, len(a), n)]
-        return pages
+        #n = 9   # Number of albums per page.
+        #pages = [a[x:x+n] for x in range(0, len(a), n)]
+        #self.album_pages = pages
+        self.album_pages = self.albums[0:9]
+        return
+
+    def set_albums(self, albums):
+        for button, i in zip(grid_layout_widget.children, range(len(grid_layout_widget.children))):
+            if albums.iloc[i] is not None:
+                button.text = albums.iloc[i][1]
+
 
 
 class MyManager(ScreenManager):
@@ -197,3 +256,10 @@ class SubiboxApp(App):
 
 if __name__ == '__main__':
     SubiboxApp().run()
+
+
+# CRUFT
+
+#for button in self.walk():
+#    #print("{} -> {}".format(button, button.id))
+#    button.color = [random.random() for _ in range(3)] + [1]
