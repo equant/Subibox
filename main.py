3b1879158d754643b7cb15cce1db8b8e2985ffa0
Nathanial Hendler <nathanhendler@gmail.com>
2017-07-01 13:45:13 -0700

    foo

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ class Album(RelativeLayout):
     def album_changed(self, instance, value):
         if len(self.album['album_art']) > 0:
             # There is an album cover image
-            ac = self.album['album_art'].replace('/mnt/jukebox/','/home/equant/')
+            #ac = self.album['album_art'].replace('/mnt/jukebox/','/home/equant/')
             album_cover = Image(source=ac, id='album_cover')
         else:
             album_cover = Label(text=self.album['full_album_name'], id='album_cover')
